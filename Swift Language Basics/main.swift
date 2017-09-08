9ec58046cc934158d689a1e2b9182fcee96b40ca
Chris Jansson <cgj6hb@mail.missouri.edu>
2017-09-08 13:57:42 -0500

    Initial Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Chris Jansson on 9/8/17.
+//  Copyright © 2017 Chris Jansson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
